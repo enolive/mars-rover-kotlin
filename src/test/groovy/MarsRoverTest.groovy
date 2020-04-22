7a5de4b6b9fb86b719823905ec69ecfe460c1e0e
Christoph Welcz <christoph@welcz.de>
2020-04-22 13:19:35 +0200

    basic setup

--- src/test/groovy/MarsRoverTest.groovy
+++ src/test/groovy/MarsRoverTest.groovy
@@ -1,6 +1,20 @@
 import groovy.transform.CompileDynamic
 import spock.lang.Specification
 
+// ffffffffffbbbbrrlll
+// Rover
+// - pos (x, y)
+// - direction (NSEW)
+
+// 1. init
+// 2. single commands
+//  - forward
+//  - backward
+//  - left
+//  - right
+// 3. Char -> command
+// 4. Multiple commands
+
 @CompileDynamic
 class MarsRoverTest extends Specification {
   def "initialization"() {
